94c59ff1f92d4bf819df372600767e4cb3460be4
gurumukhprajapati <lokeshgouravbff@gmail.com>
2025-01-26 20:31:48 +0530

    demo

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = JSON.parse(localStorage.getItem('cart')) ?? [];
-
+// hello 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
